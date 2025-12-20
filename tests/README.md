dbf27f368e0a2955dedba3ae47f99472c9cb1c69
Loris <loris.rion@gmail.com>
2025-12-20 15:45:24 +0100

    Delete tests directory

--- tests/README.md
+++
@@ -1,3 +0,0 @@
-The files in this folder are intended for use in integration auto-tests.
-
-All credentials are strictly for testing purposes only.
--- tests/graphite/statsd/config.js
+++
@@ -1,7 +0,0 @@
-{
-  "graphiteHost": "onlyoffice-graphite",
-  "graphitePort": 2003,
-  "port": 8125,
-  "flushInterval": 60000,
-  "backends": [ "./backends/graphite.js" ]
-}
